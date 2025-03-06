ffc0ade992333c618f90b6da2a708d12224cb681
rmmuk <rishi.mukherjee@student.unimelb.edu.au>
2025-03-06 11:21:13 +1100

    Fixed Calculator

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,21 +3,22 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 4;
+        int sum = a + b;
+        return sum;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        int sum = a - b;
+        return sum;
     }
 
     public int multiply(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        return a * b;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
-        return 5;
+
+        return a / b;
     }
 }
